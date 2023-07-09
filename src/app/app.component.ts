72678b8efb189e6e037f1f0b983d2de08fd7150a
MikaValjakka <mika.valjakka@hotmail.com>
2023-07-09 09:51:23 +0300

    Simple Todo-app with Angulars basic directives 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	deleted:    src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngIntro';
+
+  todoArray:string[] = [
+    "Clean house",
+    "Feed Cat",
+    "Hire Mika Vee"
+  ]
+
+  newTodo:string= '';
+  
+
+  // just for debugging and testing
+ indexNumber: number = 0;
+  
+
+
+  addNew(){
+    
+    // just for debugging and testing
+    this.indexNumber = this.todoArray.length + 1;
+    console.log("Array Index Number: " + this.indexNumber);
+    // Testing code ended, go ahead...
+    if(this.newTodo){
+      this.todoArray.push(this.newTodo);
+      this.newTodo= '';
+    }
+    
+  }
+
+  onDelete(i:number){
+    this.todoArray.splice(i, 1);
+  }
+  
+
+
 }
